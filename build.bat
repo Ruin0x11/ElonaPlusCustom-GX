c83932617090672aee56446fd398ff67b918232c
Ruin0x11 <ipickering2@gmail.com>
2021-07-31 22:26:55 -0700

    Debug assets error message

--- build.bat
+++
@@ -1,6 +0,0 @@
-rem HSP is stupid and doesn't recognize #include directives with 
-rem subdirectories unless the working directory is the root of the source folder.
-
-pushd 1.90.4.2-custom-g
-hspcmp.exe main.hsp
-popd
